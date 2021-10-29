23fd571f47dc18ed84c19fbdf55b029b6e9a1d24
Lisztaganx <93394792+Lisztaganx@users.noreply.github.com>
2021-10-29 15:43:32 +0000

    Add files via upload

---
+++ thing.py
@@ -0,0 +1,9 @@
+print('Hello, do you exist?')
+existence = input('Type yes if you exist or no if you do not exist')
+if existence == 'yes':
+    print('Congratulations, you exist')
+elif existence == 'no':
+    print('Oh no, you do not exist :(')
+else:
+    print('ANSWER THE QUESTION FFS')
+# Credits to Lisztaganx (me) for writing such a magnificent python script
